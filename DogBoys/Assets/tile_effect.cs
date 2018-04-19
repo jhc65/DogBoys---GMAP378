ad0f79560ed6627367dcd27bedd7040ef610cc45
Tom Dragna <tcd53@drexel.edu>
2018-04-19 15:30:01 -0400

    tech demo - grid test
    12x12 grid/ mouseover highlights a square, mouseout removes the effect

---
+++ DogBoys/Assets/tile_effect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile_effect : MonoBehaviour {
+
+	public bool on;
+
+	// Use this for initialization
+	void Start () {
+		on = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (on == false)
+			Destroy (this.gameObject);
+	}
+}
---
+++ DogBoys/Assets/tile_property.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile_property : MonoBehaviour {
+
+	public GameObject mouseover_effect;
+	private Vector3 my_position;
+	private bool effect_status;
+	private GameObject my_effect;
+	// Use this for initialization
+	void Start () {
+		my_position = this.gameObject.transform.position;
+		effect_status = false;
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+
+	void OnMouseOver(){
+		if (effect_status == false) {
+			Debug.Log ("Mouse is over" + this.name);
+			my_effect = Instantiate (mouseover_effect, my_position, transform.rotation);
+			effect_status = true;
+		}
+	}
+
+	void OnMouseExit(){
+		Debug.Log("Mouse has left" + this.name);
+		effect_status = false;
+		Destroy (my_effect.gameObject);
+	}
+
+}
