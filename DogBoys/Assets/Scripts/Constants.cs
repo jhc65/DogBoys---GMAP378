b35dbfd3035ee407e458f50d5f41e12df04f0fd4
Joe <jhc65@drexel.edu>
2018-04-27 11:21:16 -0400

    - attached camera script to Camera - added Constants file

---
+++ DogBoys/Assets/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants {
+    // Camera Values
+    public static class CameraStats {
+        public static float C_cameraMoveSpeed = 20.0f;
+    }
+}
