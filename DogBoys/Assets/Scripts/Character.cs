822a6c6d546decab0ccd33c87816a559dafcc4f4
Joe <jhc65@drexel.edu>
2018-04-27 00:40:03 -0400

    - almost working movement functionality - added singleton game controller

--- DogBoys/Assets/Scripts/Character.cs
+++ DogBoys/Assets/Scripts/Character.cs
@@ -10,12 +10,18 @@ public class Character : MonoBehaviour {
 	private string status;
     [SerializeField]
 	private string weapon; //TODO: weapon class
+    [SerializeField]
+    private GameObject characterHUD;
+
     private bool canMove = false;
     private bool isMoving = false;
+    private bool isSelected = false;
 	private int xPos, yPos;
+    private Vector3 newPos;
+
+    private GameController gc;
 
     #region Getters and Setters
-    public 
     #endregion
     #endregion
 
@@ -25,28 +31,82 @@ public class Character : MonoBehaviour {
         Destroy(gameObject);
     }
 
-    void Move(int x, int y)
+    public void Move(Vector3 position)
     {
-        xPos = x;
-        yPos = y;
-        canMove = false;
+        //newPos = new Vector3(position.x, gameObject.transform.position.y, position.z);
+        //gameObject.transform.position = newPos;
+        if (canMove) {
+            newPos = new Vector3(position.x, gameObject.transform.position.y, position.z);
+            isMoving = true;
+        }
+    }
+
+    private void CenterOnSpace() {
+        // Don't collide with Player layer
+        //int layerMask = 1 << 8;
+
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.down) * 3f, out hit, Mathf.Infinity)) {
+            //Debug.DrawRay(gameObject.transform.position, gameObject.transform.TransformDirection(Vector3.down) * 3f, Color.red);
+            Vector3 centered = new Vector3(hit.transform.position.x, gameObject.transform.position.y, hit.transform.position.z);
+            gameObject.transform.position = centered;
+        }
+    }
+
+    private void ToggleIsSelected() {
+        if (isSelected) {
+            isSelected = false;
+        }
+        else {
+            isSelected = true;
+        }
+    }
+
+    private void SetIsSelected(bool inSelection) {
+        isSelected = inSelection;
+    }
+
+    private void SelectCharacter() {
+        SetIsSelected(true);
+        characterHUD.SetActive(true);
+        gc.SetSelectedCharacter(gameObject);
+    }
+
+    public void UnselectCharacter() {
+        SetIsSelected(false);
+        characterHUD.SetActive(false);
     }
     #endregion
 
     #region Unity Overrides
     // Use this for initialization
     void Start () {
+        gc = GameController.Instance;
 		canMove = true;
 		health = 100;
 		status = "";
 		weapon = "";
-	}
+
+        CenterOnSpace();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (health <= 0) {
 			Die ();
 		}
-	}
+
+        if (!isMoving && Input.GetMouseButtonDown(0)) {
+            SelectCharacter();
+        }
+        if (isMoving) {
+            if (gameObject.transform.position != newPos) {
+                Vector3.Lerp(gameObject.transform.position, newPos, (Time.deltaTime * 0.5f));
+            }
+            else {
+                isMoving = false;
+            }
+        }
+    }
     #endregion
 }
---
+++ DogBoys/Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    #region Variables and Declarations
+    private static GameController instance;
+    
+    private GameObject currentlySelectedCharacter = null;
+
+    #region Getters and Setters
+    public static GameController Instance {
+        get { return instance; }
+    }
+    #endregion
+    #endregion
+
+    #region GameController Custom Methods
+    public bool HasSelectedCharacter() {
+        if (currentlySelectedCharacter) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void SetSelectedCharacter(GameObject charIn) {
+        currentlySelectedCharacter = charIn;
+    }
+
+    private void UnselectCharacter() {
+        currentlySelectedCharacter.GetComponent<Character>().UnselectCharacter();
+        currentlySelectedCharacter = null;
+    }
+
+    public void MoveSelectedCharacter(Vector3 position) {
+        if (currentlySelectedCharacter) {
+            currentlySelectedCharacter.GetComponent<Character>().Move(position);
+        }
+    }
+    #endregion
+
+    #region Unity Overrides
+    // Use this for initialization
+    void Start() {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            UnselectCharacter();
+        }
+    }
+    #endregion
+}
