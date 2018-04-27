7ba07aac995cb711a90471fd5e4fbd141a39421d
Jordan Bodisch <jmb669@drexel.edu>
2018-04-26 22:46:14 -0400

    scene changing
    Added menu nav functionality

---
+++ DogBoys/Assets/Scripts/menu_control.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_control : MonoBehaviour {
+
+	public void loadMission() {
+		SceneManager.LoadScene ("TechDemo-Tom");
+	}
+
+	public void exit() {
+		Application.Quit ();
+	}
+		
+}
