885edc98df6375a236903929f08e9059cd3cf6b3
Tom Dragna <tcd53@drexel.edu>
2018-05-23 21:45:32 -0400

    new revolver dog prefab
    work in progress. needs hud setup and to be attackable

--- DogBoys/Assets/Scripts/menu_control.cs
+++ DogBoys/Assets/Scripts/menu_control.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class menu_control : MonoBehaviour {
 
 	public void loadMission() {
-		SceneManager.LoadScene ("Main Scene");
+		SceneManager.LoadScene ("Level01");
 	}
 
 	public void exit() {
