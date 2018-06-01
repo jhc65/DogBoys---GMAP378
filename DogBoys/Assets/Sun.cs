999131fb97fa6f832b6ad439932efa01056ee60d
Jordan Bodisch <jmb669@drexel.edu>
2018-05-31 22:26:31 -0400

    spinny sun
    spinnny

---
+++ DogBoys/Assets/Sun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+	[SerializeField]
+	private float deltaPhi;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (Vector3.right * deltaPhi * Time.deltaTime);
+	}
+}
