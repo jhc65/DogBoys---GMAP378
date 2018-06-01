9bf9443bef3345b5cc45af4817ca10fdcc408ed3
Joe <jhc65@drexel.edu>
2018-06-01 01:04:51 -0400

    - cover should be working now

---
+++ DogBoys/Assets/Scripts/CoverTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverTile : MonoBehaviour {
+    #region Variables and Declarations
+    [SerializeField]
+    private Constants.Global.C_CoverDirection dirCoveringPlayer;
+    #endregion
+
+    #region CoverTile Methods
+    public Constants.Global.C_CoverDirection GetDirection() {
+        return dirCoveringPlayer;
+    }
+    #endregion
+
+    #region Unity Overrides
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+}
