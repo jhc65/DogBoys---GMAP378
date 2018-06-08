2e6f753265fc812ccafdfda1072be3fa90e1b784
Jordan Bodisch <jmb669@drexel.edu>
2018-06-08 02:24:30 -0400

    sounds for shootin and reloadin

--- DogBoys/Assets/Sun.cs
+++ DogBoys/Assets/Sun.cs
@@ -6,14 +6,18 @@ public class Sun : MonoBehaviour {
 
 	[SerializeField]
 	private float deltaPhi;
+	private Quaternion initial;
 
 	// Use this for initialization
 	void Start () {
-		
+		initial = gameObject.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.Rotate (Vector3.right * deltaPhi * Time.deltaTime);
+		if (gameObject.transform.rotation.x > 145) {
+			gameObject.transform.rotation = initial;
+		}
 	}
 }
